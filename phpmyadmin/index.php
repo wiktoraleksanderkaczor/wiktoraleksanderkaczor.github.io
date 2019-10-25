ac8e339519a677275f079385ad70e81fe51f37fb
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2019-10-25 17:29:41 +0100

    Initial commit

---
+++ phpmyadmin/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<h1>Hello</h1>
+		<p>I'm going to have to ask you to go back to the main page and stop looking for possible administration pages. :)</p>
+	</body>
+</html>
+
