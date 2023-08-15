2937920b8c0246a34fa0068c6c4a422f9c10e3d7
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2023-08-15 06:16:25 +0100

    Remove bad analytics addition

--- articles/publishconf.py
+++ articles/publishconf.py
@@ -19,16 +19,3 @@ DELETE_OUTPUT_DIRECTORY = True
 
 #DISQUS_SITENAME = ""
 GOOGLE_ANALYTICS = "G-V4JGBBCE6G"
-
-# Following does not work:
-# ANALYTICS = """
-# <!-- Google tag (gtag.js) -->
-# <script async src="https://www.googletagmanager.com/gtag/js?id=G-V4JGBBCE6G"></script>
-# <script>
-#   window.dataLayer = window.dataLayer || [];
-#   function gtag(){dataLayer.push(arguments);}
-#   gtag('js', new Date());
-
-#   gtag('config', 'G-V4JGBBCE6G');
-# </script>
-# """
